c551bd9f9d1bfc551f08dd1a8d41bb042157204e
Camila Teran <camila@MacBook-Air-de-Camila.local>
2020-10-12 22:35:47 -0500

    Inicio registran

--- st-parranderos-jdo-est/st-parranderos-jdo-est/src/main/java/uniandes/isis2304/parranderos/persistencia/SQLRegistranVehiculo.java
+++ st-parranderos-jdo-est/st-parranderos-jdo-est/src/main/java/uniandes/isis2304/parranderos/persistencia/SQLRegistranVehiculo.java
@@ -14,5 +14,33 @@ package uniandes.isis2304.parranderos.persistencia;
  */
 public class SQLRegistranVehiculo 
 {
+	/* ****************************************************************
+	 * 			Constantes
+	 *****************************************************************/
+	/**
+	 * Cadena que representa el tipo de consulta que se va a realizar en las sentencias de acceso a la base de datos
+	 * Se renombra acá para facilitar la escritura de las sentencias
+	 */
+	private final static String SQL = PersistenciaAforoAndes.SQL;
+
+	/* ****************************************************************
+	 * 			Atributos
+	 *****************************************************************/
+	/**
+	 * El manejador de persistencia general de la aplicación
+	 */
+	private PersistenciaAforoAndes pp;
+
+	/* ****************************************************************
+	 * 			Métodos
+	 *****************************************************************/
+	/**
+	 * Constructor
+	 * @param pp - El Manejador de persistencia de la aplicación
+	 */
+	public SQLRegistranVehiculo (PersistenciaAforoAndes pp)
+	{
+		this.pp = pp;
+	}
 
 }
